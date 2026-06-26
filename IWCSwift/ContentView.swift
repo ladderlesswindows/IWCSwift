68a225b6656465185ea9de2adee8bb59c74a5c3d
CV <sharksoftwash@outlook.com>
2026-06-26 07:45:57 -0700

    Initial Commit

---
+++ IWCSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IWCSwift
+//
+//  Created by C Zilla on 6/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IWCSwift/IWCSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IWCSwiftApp.swift
+//  IWCSwift
+//
+//  Created by C Zilla on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct IWCSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
